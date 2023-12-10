0e97cf1e434d93684116fe34e40f7cfa7cddfc6d
LVNguyenn <levinguyen170602@gmail.com>
2023-12-10 20:51:39 +0700

    Get all insurance, get a insurance by id, add a insurance

---
+++ Domain/Insurance.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InsuranceManagement.Domain
+{
+    public class Insurance
+    {
+        public Guid id { get; set; }
+
+        public string title { get; set; }
+
+        public int price { get; set; }
+
+        public string description { get; set; }
+
+        public string period { get; set; }
+
+        public string image { get; set; }
+    }
+}
