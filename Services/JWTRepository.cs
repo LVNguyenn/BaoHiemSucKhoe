81567af864fec6ad5d99c72bb3ff730a70e4ace0
Trương Công Quốc Cường <59004156+TeoLeoGit@users.noreply.github.com>
2024-01-10 01:04:29 +0700

    Add JWTRepository

--- Services/JWTRepository.cs
+++ Services/JWTRepository.cs
@@ -1,6 +1,55 @@
-﻿namespace InsuranceManagement.Services
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace InsuranceManagement.Services
 {
-    public class JWTRepository
+    public interface ITokenRepository
     {
+        string CreateJWTToken(IdentityUser user, List<string> roles);
     }
+
+    public class JWTRepository : ITokenRepository
+    {
+        private readonly IConfiguration configuration;
+
+        public JWTRepository(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        public string CreateJWTToken(IdentityUser user, List<string> roles)
+        {
+            // Create claim
+            var claim = new List<Claim>();
+            claim.Add(new Claim(ClaimTypes.Email, user.Email));
+
+            foreach (var role in roles)
+            {
+                claim.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            // create key
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]));
+
+            // create credentials 
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            // create token
+
+            var token = new JwtSecurityToken(
+                configuration["Jwt:Issuer"],
+                configuration["Jwt:Audience"],
+                claim,
+                expires: DateTime.Now.AddMinutes(15),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+
 }
